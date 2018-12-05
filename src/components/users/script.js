57ef5f44d5717b3d284ca996df5e9b0f654c855c
jiaoliu0111 <2805115506@qq.com>
2018-12-05 19:49:24 +0800

    分页完成

--- src/components/users/script.js
+++ src/components/users/script.js
@@ -7,27 +7,41 @@ export default {
       // email,
       // tel
       total: 0,
-      pagesize: 3,
+      pagesize: 2,
       pagenum: 1
     }
   },
   created () {
-    axios.get('http://localhost:8888/api/private/v1/users', {
-      params: {
-        query: '',
-        pagenum: 1,
-        pagesize: 3
-      },
-      headers: {
-        Authorization: localStorage.getItem('token')
-      }
-    }).then(res => {
-      console.log(res)
-      if (res.data.meta.status === 200) {
-        this.usersData = res.data.data.users
-        this.total = res.data.total
-        this.pagenum = res.data.pagenum
-      }
-    })
+    this.getUsersList() // ???this指代什么
+  },
+  methods: {
+    getUsersList (pagenum = 1, pagesize = 2, query = '') {
+      axios.get('http://localhost:8888/api/private/v1/users', {
+        params: {
+          query,
+          pagenum,
+          pagesize
+        },
+        headers: {
+          Authorization: localStorage.getItem('token')
+        }
+      }).then(res => {
+        console.log(res)
+        if (res.data.meta.status === 200) {
+          this.usersData = res.data.data.users
+          this.total = res.data.data.total
+          this.pagenum = res.data.data.pagenum
+        } else {
+          this.$router.push('/login')
+          localStorage.removeItem('token')
+        }
+      })
+    },
+    //  切换页码
+    changePage (curPage) {
+      console.log('当前页是：', curPage)
+      this.getUsersList(curPage) // 1- 切换渲染新页面 2- 在请求渲染成功的回调里面把返回的pagenum赋值给动态绑定的current-page
+    }
+
   }
 }
